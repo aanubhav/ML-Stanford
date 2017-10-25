49bff4effab4301f4360a6c51499c3da519883be
aanubhav <anubhavanandsingh@gmail.com>
2017-10-25 19:59:04 +0530

    Neural Network Prediction Function Accuracy: 97.52 %

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -7,12 +7,12 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -20,6 +20,35 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
+X = [ones(m, 1) X];  %add bias
+for i=1:m
+a1 = zeros(size(X,2),1);
+a1 = X(i,:)';
+
+z2 = zeros(size(Theta1),1);
+z2 = Theta1 * a1;
+
+a2 = zeros(size(Theta1),1);
+a2 = sigmoid(z2);
+a2 = a2'; %add bias
+a2 = [1 a2];
+a2 = a2';
+
+z3 = zeros(size(Theta2),1);
+z3 = Theta2 * a2;
+
+a3 = zeros(size(Theta2),1);
+a3 = sigmoid(z3);
+
+
+[num pos] = max(a3);
+p(i) = pos;
+
+
+
+
+
+end
 
 
 
